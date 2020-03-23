2ace89265ba2e31f78a3072f126e314e48b1877d
vungocson <vungocson998@gmail.com>
2020-03-23 20:46:41 +0700

    Updates

---
+++ precache-manifest.305d28d137d449e9ef7540b4fec9490c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1d893f882acf444895633ce94677de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2b7c47767caa2efe4a2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2046ab1142e8ce74146d",
+    "url": "/static/js/2.0793ffb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0793ffb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2b7c47767caa2efe4a2",
+    "url": "/static/js/main.1d06fafc.chunk.js"
+  },
+  {
+    "revision": "d42a603f0f0be8948605",
+    "url": "/static/js/runtime-main.d303fdb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
