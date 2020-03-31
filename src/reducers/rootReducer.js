bf441a6e25a2a746340d394713a911184889be2c
vungocson <vungocson998@gmail.com>
2020-03-31 15:02:41 +0700

    set up redux

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,20 @@
+const initState = {
+    todos: [
+        {id: '', name: '', detail: '', time: ''},
+        {id: '', name: '', detail: '', time: ''},
+        {id: '', name: '', detail: '', time: ''},
+        {id: '', name: '', detail: '', time: ''}
+    ],
+    done:[
+        {id: '', name: '', detail: '', time: ''},
+        {id: '', name: '', detail: '', time: ''},
+        {id: '', name: '', detail: '', time: ''},
+        {id: '', name: '', detail: '', time: ''},
+    ]
+}
+
+const rootReducer = (state = initState, action) => {
+    return state;
+}
+
+export default rootReducerl
